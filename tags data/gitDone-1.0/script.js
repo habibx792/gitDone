f714dbbe347aa2e457bfb6c3c8460314482e6be4
Ghulam habib <ghulamhaib.x@gmail.com>
2025-08-30 16:31:15 +0500

    done

---
+++ tags data/gitDone-1.0/script.js
@@ -0,0 +1,16 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+console.log("Hello i am working with git  \n and i am doint well");
+let i=1000;
+while(i>=0)
+{
+	console.log("i love the coding");
+	console.log(i+100);
+         i--;
+}
+=======
+console.log("Hello i am working with git  \n and i am doint well");
+>>>>>>> Stashed changes
+=======
+console.log("Hello i am working with git  \n and i am doint well");
+>>>>>>> Stashed changes
